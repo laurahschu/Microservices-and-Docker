140b6429645786df6519308837a8ebf3c3e65b4f
Laura <contaclauraschu@gmail.com>
2024-11-17 20:51:03 -0300

    first commit

---
+++ api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,41 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+	@Bean
+	RouteLocator getRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(rota -> rota
+					.path("/get")
+					.filters(f -> f 
+							.addRequestHeader("Usuario", "Laura Schu")
+							.addRequestParameter("Parametro", "valorParametro")
+							.addResponseHeader("Servidor", "meuServer"))
+					.uri("http://httpbin.org"))
+			.route(rota -> rota
+					.path("/cambio-service/**")
+					.filters(f -> f
+							.addRequestHeader("Usuario", "Laura Schu"))
+					.uri("lb://cambio-service"))
+			.route(rota -> rota
+					.path("/produto-service/**")
+					.filters(f -> f
+							.addRequestHeader("Usuario", "Laura Schu"))
+					.uri("lb://produto-service"))
+			.route(rota -> rota
+					.path("/saudacao-service/**")
+					.filters(f -> f
+							.addRequestHeader("Usuario", "Laura Schu"))
+					.uri("lb://saudacao-service"))
+			.build();
+	}
+	
+	
+	
+}
